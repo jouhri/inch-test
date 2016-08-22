53eb5351b057b43279021572c742c70ac4d80147
jouhri <jouhri.oualid@gmail.com>
2016-08-22 20:03:38 +0200

    add config file

--- lib/geronimo/redis.rb
+++ lib/geronimo/redis.rb
@@ -1,19 +1,21 @@
 module Geronimo
   module Redis
-    attributes = [:manager_name, :email, :address, :mobile_phone_number, :home_phone_number]
-    attributes.each do |method_name|
-      define_method method_name do
-        $redis.zrange("Person:#{reference}:#{method_name}", -1, -1).first
-      end
-    end
-
     module ClassMethods
     end
 
     module InstanceMethods
+      @@whitelist = YAML::load(File.open("#{Rails.root}/config/whitelist.yml"))
+
+      @@whitelist.each do |model, attributes|
+        attributes.each do |method_name|
+          define_method method_name do
+            $redis.zrange("#{model}:#{reference}:#{method_name}", -1, -1).first
+          end
+        end
+      end
+
       def update_history
-        @whitelist = ["manager_name", "email", "address", "mobile_phone_number", "home_phone_number"]
-        @whitelist.each do | attribute |
+        @@whitelist[self.class.to_s].each do | attribute |
           key = "#{self.class.to_s}:#{self.reference}:#{attribute}"
           unless $redis.zscore(key, self.attributes[attribute])
             index = $redis.zcard(key)
